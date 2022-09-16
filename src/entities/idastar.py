dc1809b70fc6100edec2c73e9183a91109443c74
DeeCaaD <perttu.kangas@hotmail.fi>
2022-09-16 18:08:52 +0300

    Sketch for algorithms and their ticker

---
+++ src/entities/idastar.py
@@ -0,0 +1,15 @@
+class IDAStar:
+
+    def __init__(self, grid, start, end):
+        self.grid = grid
+        self.start = start
+        self.end = end
+
+    def find(self):
+        return -1
+
+    def step(self):
+        return -1
+
+    def final_path(self):
+        return -1
---
+++ src/services/algorithm_ticker.py
@@ -0,0 +1,17 @@
+import time
+
+
+class AlgorithmTicker:
+
+    def __init__(self, grid, algorithm):
+        self.grid = grid
+        self.algorithm = algorithm
+
+    def start(self):
+        return -1
+
+    def tick(self):
+        return -1
+
+    def draw(self):
+        return -1
